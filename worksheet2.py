baabf1fc57fd55c6228ff93dc4c43f60ab45569d
ujjwx1 <uaggarwal1_be23@thapar.edu>
2024-09-15 18:22:53 +0530

    Add files via upload

--- worksheet2.py
+++ worksheet2.py
@@ -1,7 +1,7 @@
 L = [11, 12, 13, 14]
 
 # (i) 
-L.extend([50, 60])
+L += [50, 60]
 
 # (ii) 
 L.remove(11)
@@ -14,38 +14,25 @@ L.sort()
 L.sort(reverse=True)
 
 # (v)
-if 13 in L:
-    print("13 is in the list")
-else:
-    print("13 is not in the list")
+print("13 is in the list" if 13 in L else "13 is not in the list")
 
 # (vi) 
-num_elements = len(L)
-print("Number of elements:", num_elements)
+print("Number of elements:", len(L))
 
 # (vii)
-total_sum = sum(L)
-print("Sum of all elements:", total_sum)
+print("Sum of all elements:", sum(L))
 
 # (viii) 
-odd_sum = sum(x for x in L if x % 2 != 0)
-print("Sum of odd numbers:", odd_sum)
+print("Sum of odd numbers:", sum(x for x in L if x % 2 != 0))
 
 # (ix) 
-even_sum = sum(x for x in L if x % 2 == 0)
-print("Sum of even numbers:", even_sum)
+print("Sum of even numbers:", sum(x for x in L if x % 2 == 0))
 
 # (x) 
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+def is_prime(n):
+    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
 
-prime_sum = sum(x for x in L if is_prime(x))
-print("Sum of prime numbers:", prime_sum)
+print("Sum of prime numbers:", sum(x for x in L if is_prime(x)))
 
 # (xi) 
 L.clear()
@@ -53,12 +40,9 @@ L.clear()
 # (xii) 
 del L
 
-
+# Functions for sum and multiplication
 def sum_list(lst):
-    total = 0
-    for num in lst:
-        total += num
-    return total
+    return sum(lst)
 
 def multiply_list(lst):
     product = 1
@@ -66,11 +50,10 @@ def multiply_list(lst):
         product *= num
     return product
 
-# Q 2&3
+# Q 2 & 3
 my_list = [1, 2, 3, 4, 5]
 print(sum_list(my_list))  
-print(multiply_list(my_list)) 
-
+print(multiply_list(my_list))
 
 D = {1: 5.6, 2: 7.8, 3: 6.6, 4: 8.7, 5: 7.7}
 
@@ -81,18 +64,13 @@ D[8] = 8.8
 del D[2]
 
 # (iii) 
-if 6 in D:
-    print("Key 6 is present.")
-else:
-    print("Key 6 is not present.")
+print("Key 6 is present." if 6 in D else "Key 6 is not present.")
 
 # (iv)
-count = len(D)
-print("Number of elements:", count)
+print("Number of elements:", len(D))
 
 # (v)
-sum_values = sum(D.values())
-print("Sum of values:", sum_values)
+print("Sum of values:", sum(D.values()))
 
 # (vi)
 D[3] = 7.1
@@ -100,23 +78,17 @@ D[3] = 7.1
 # (vii) 
 D.clear()
 
-
 S1 = {10, 20, 30, 40, 50, 60}
 S2 = {40, 50, 60, 70, 80, 90}
 
 # (i)
-S1.add(55)
-S1.add(66)
+S1 |= {55, 66}
 
 # (ii) 
-S1.remove(10)
-S1.remove(30)
+S1 -= {10, 30}
 
 # (iii) 
-if 40 in S1:
-    print("40 is present in S1.")
-else:
-    print("40 is not present in S1.")
+print("40 is present in S1." if 40 in S1 else "40 is not present in S1.")
 
 # (iv) 
 union_set = S1 | S2
@@ -127,21 +99,14 @@ intersection_set = S1 & S2
 # (vi) 
 difference_set = S1 - S2
 
-
-
-import random
+import random, string
 
 # (i) 
 for _ in range(100):
-    length = random.randint(6, 8)
-    string = ''.join(random.choices(string.ascii_letters, k=length))
-    print(string)
+    print(''.join(random.choices(string.ascii_letters, k=random.randint(6, 8))))
 
 # (ii)
-def is_prime(num):
- 
-
- for num in range(600, 801):
+for num in range(600, 801):
     if is_prime(num):
         print(num)
 
@@ -150,54 +115,27 @@ for num in range(100, 1001):
     if num % 7 == 0 and num % 9 == 0:
         print(num)
 
-
-
 exam_st_date = (11, 12, 2014)
-day, month, year = exam_st_date
-print("Exam start date:", day, "/", month, "/", year) 
-
+print("Exam start date:", *exam_st_date, sep="/")
 
 numbers = [1, 5, 10, 15, 20, 23, 25]
-for number in numbers:
-    if number % 5 == 0:
-        print(number)
+print([num for num in numbers if num % 5 == 0])
 
-
-
-def is_even(number):
-    return number % 2 == 0
+def is_even(num):
+    return num % 2 == 0
 
 num = 10
-if is_even(num):
-    print(num, "is even.")
-else:
-    print(num, "is odd.")
+print(f"{num} is even." if is_even(num) else f"{num} is odd.")
 
-
-def count_emma(string):
-    count = 0
-    for i in range(len(string) - len("Emma") + 1):
-        if string[i:i+len("Emma")] == "Emma":
-            count += 1
-    return count
+def count_emma(s):
+    return s.count("Emma")
 
 text = "Emma is girl. Emma likes to play football."
-result = count_emma(text)
-print("'Emma' appears", result, "times ")
-
-
+print("'Emma' appears", count_emma(text), "times")
 
 def create_new_list(list1, list2):
-    new_list = []
-    for num in list1:
-        if num % 2 != 0:
-            new_list.append(num)
-    for num in list2:
-        if num % 2 == 0:
-            new_list.append(num)
-    return new_list
+    return [x for x in list1 if x % 2 != 0] + [x for x in list2 if x % 2 == 0]
 
 list1 = [1, 3, 5, 7]
 list2 = [2, 4, 6, 8]
-new_list = create_new_list(list1, list2)
-print("New list:", new_list)
+print("New list:", create_new_list(list1, list2))
